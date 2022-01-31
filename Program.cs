4d4244db7f0b8ac8d13ac5d631936bbe9c49b0c3
ramameng <ramameng@gmail.com>
2022-01-31 14:52:57 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exit = 0;
+
+            while (exit == 0)
+            {
+                var numberList = Enumerable.Range(0, 101).ToList();
+
+                foreach (var number in numberList)
+                {
+                    Console.WriteLine(Checker(number));
+                }
+
+                Console.WriteLine("");
+                Console.Write("Try again? (Press n to exit.) ");
+                if (Console.ReadKey().Key == ConsoleKey.N)
+                    exit = 1;
+                else
+                    Console.WriteLine("");
+            }
+        }
+
+        static string Checker(int number)
+        {
+            if (number % 3 == 0)
+            {
+                if (number % 5 == 0)
+                    return "FizzBuzz";
+                else return "Fizz";
+            }
+            else if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+- There is no input needed for this app. I'ts automatic show the result from list numbers of 0 - 100.
